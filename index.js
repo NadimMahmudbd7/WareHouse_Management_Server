0765a2bc9c5f75291652e85cc7c95a22203c2c97
Nadim Mahmud <giftsframebd@gmail.com>
2022-05-01 21:43:32 +0600

    first one

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 4000
+
+app.use(cors())
+app.use(express.json())
+
+
+async function run(){
+    
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.ambpg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log("connected db");
+    try{
+        await client.connect()
+        const WaltonCollections = client.db("WaltonCollection").collection("Products")
+
+        app.get("/",(req,res)=>{
+            res.send("this is successfull")
+        })
+        
+        // for show all products
+        app.get('/products',async(req,res)=>{
+            const query = {}
+            const filter ={}
+            const cursor = WaltonCollections.find(query)
+            const products = await cursor.limit(6). toArray()
+            const  findAllProducts = WaltonCollections.find(filter)
+            const allProducts = await findAllProducts.toArray()
+            res.send({success:"successfully loaded", products:products, allProducts:allProducts})
+        })
+
+        // for update products
+        app.get("/updateproduct/:id",async(req,res)=>{
+            const id = req.params.id
+            const query = {_id:ObjectId(id)}
+            const product = await WaltonCollections.findOne(query)
+            res.send({success:"find Successfullu", product:product})
+        })
+
+        app.get("")
+
+        // for deliver products
+        app.put("/updateproduct/:id",async(req,res)=>{
+            const id = req.params.id
+            let previusQty=parseInt(req.body.qty)
+            const deliverQty = parseInt(req.body.deliverQty)
+            previusQty = previusQty-deliverQty
+            const filter = {_id:ObjectId(id)}
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    qty:previusQty
+                }
+              };
+              const result = await WaltonCollections.updateOne(filter,updateDoc,options)
+              console.log();
+            res.send({succrssfull:"successfull",result:result})            
+        })
+
+        app.post("/products", async(req,res)=>{
+            const newitem = req.body
+            const result = await WaltonCollections.insertOne(newitem)
+            console.log("add",result);
+            res.send(result)
+        })
+
+        // for Delete
+        app.delete('/products/:id',async(req,res)=>{
+            const id = req.params.id
+            console.log(id);
+            const query = {_id:ObjectId(id)}
+            const result = await WaltonCollections.deleteOne(query)
+            console.log("deleted", result);
+            res.send(result)
+        })
+
+        // for update products
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir)
+
+app.listen(port,()=>{
+    console.log("Listening port",port);
+})
