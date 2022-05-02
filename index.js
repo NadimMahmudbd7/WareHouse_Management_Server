2d21d6f2e7eab579ef90dc274b3cd84ffe50af4f
Nadim Mahmud <giftsframebd@gmail.com>
2022-05-02 14:44:12 +0600

    jtoken half complete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
+var jwt = require('jsonwebtoken');
 const app = express()
 const port = process.env.PORT || 4000
 
@@ -8,119 +9,135 @@ app.use(cors())
 app.use(express.json())
 
 
-async function run(){
-    
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.ambpg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log("connected db");
-    try{
+async function run() {
+
+    const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+    const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.ambpg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+    console.log("connected db");
+    try {
         await client.connect()
         const WaltonCollections = client.db("WaltonCollection").collection("Products")
-        const AllUsers = client.db("WaltonCollection").collection("users")
 
-        app.get("/",(req,res)=>{
-            res.send("this is successfull")
+        app.get("/", (req, res) => {
+            res.send("This website Is Running")
         })
-        
-        // for show all products
-        app.get('/products',async(req,res)=>{
+
+
+
+        // ..................................// For All Products //...........................................................//
+
+        app.get('/products', async (req, res) => {
             const query = {}
-            const filter ={}
+            const filter = {}
             const cursor = WaltonCollections.find(query)
-            const products = await cursor.limit(6). toArray()
-            const  findAllProducts = WaltonCollections.find(filter)
+            const products = await cursor.limit(6).toArray()
+            const findAllProducts = WaltonCollections.find(filter)
             const allProducts = await findAllProducts.toArray()
-            res.send({success:"successfully loaded", products:products, allProducts:allProducts})
+            res.send({ success: "successfully loaded", products: products, allProducts: allProducts })
         })
 
-        // for update products
-        app.get("/updateproduct/:id",async(req,res)=>{
+
+        // ..................................// For Update Products //...........................................................//
+
+        app.get("/updateproduct/:id", async (req, res) => {
             const id = req.params.id
-            const query = {_id:ObjectId(id)}
+            const query = { _id: ObjectId(id) }
             const product = await WaltonCollections.findOne(query)
-            res.send({success:"find Successfullu", product:product})
+            res.send({ success: "find Successfullu", product: product })
         })
 
 
-        // for deliver products
-        app.put("/updateproduct/:id",async(req,res)=>{
+        // ..................................// Deliver Products Quentity //...........................................................//
+
+        app.put("/updateproduct/:id", async (req, res) => {
             const id = req.params.id
             console.log(req.query.data);
-            let previusQty=parseInt(req.body.qty)
+            let previusQty = parseInt(req.body.qty)
             const deliverQty = parseInt(req.body.deliverQty)
-            previusQty = previusQty-deliverQty
-            const filter = {_id:ObjectId(id)}
+            previusQty = previusQty - deliverQty
+            const filter = { _id: ObjectId(id) }
             const options = { upsert: true };
             const updateDoc = {
                 $set: {
-                    qty:previusQty
+                    qty: previusQty
                 }
-              };
-              const result = await WaltonCollections.updateOne(filter,updateDoc,options)
-              console.log();
-            res.send({succrssfull:"successfull",result:result})            
+            };
+            const result = await WaltonCollections.updateOne(filter, updateDoc, options)
+            res.send({ succrssfull: "successfull", result: result })
         })
 
-        app.put("/updateproducts/:id",async(req,res)=>{
+
+        // ..................................// Add Products Quentity //...........................................................//
+
+        app.put("/updateproducts/:id", async (req, res) => {
             const id = req.params.id
-            let previusQty=parseInt(req.body.qty)
+            let previusQty = parseInt(req.body.qty)
             const deliverQty = parseInt(req.body.deliverQty)
-            previusQty = previusQty+deliverQty
-            const filter = {_id:ObjectId(id)}
+            previusQty = previusQty + deliverQty
+            const filter = { _id: ObjectId(id) }
             const options = { upsert: true };
             const updateDoc = {
                 $set: {
-                    qty:previusQty
+                    qty: previusQty
                 }
-              };
-              const result = await WaltonCollections.updateOne(filter,updateDoc,options)
-              console.log();
-            res.send({succrssfull:"successfull",result:result})            
+            };
+            const result = await WaltonCollections.updateOne(filter, updateDoc, options)
+            res.send({ succrssfull: "successfull", result: result })
         })
 
-        app.post("/products", async(req,res)=>{
+
+        // ..................................// Add New Product //...........................................................//
+
+        app.post("/products", async (req, res) => {
             const newitem = req.body
+            const token =req.headers.authorization
+            const[email,AccessToken] = token.split(" ")
+            if()
             const result = await WaltonCollections.insertOne(newitem)
-            console.log("add",result);
             res.send(result)
         })
 
-        // for Delete
-        app.delete('/products/:id',async(req,res)=>{
+
+        // ..................................// Delete Products //...........................................................//
+
+        app.delete('/products/:id', async (req, res) => {
             const id = req.params.id
-            console.log(id);
-            const query = {_id:ObjectId(id)}
+            const query = { _id: ObjectId(id) }
             const result = await WaltonCollections.deleteOne(query)
-            console.log("deleted", result);
             res.send(result)
         })
 
 
-        app.post("/signup", async(req,res)=>{
-            const newusers = req.body
-            const result = await AllUsers.insertOne(newusers)
-            console.log("add",result);
-            res.send(result)
-        })
 
-        // Per User Items
-        app.get("/items",async(req,res)=>{
+
+        // ..................................// Per User Item //...........................................................//
+        app.get("/items", async (req, res) => {
             const email = req.query.email
             console.log(email);
-            const query = {email}
+            const query = { email }
             const cursor = WaltonCollections.find(query)
             const users = await cursor.toArray()
-            res.send({success:"successfully added",users:users})
+            res.send({ success: "successfully added", users: users })
+        })
+
+
+        app.post("/login",(req,res)=>{
+           const email =req.body;
+           const token = jwt.sign(email, process.env.ACCESS_TOKEN);
+           console.log(token);
+           res.send({token})
         })
     }
-    finally{
+    finally {
 
     }
 }
 
 run().catch(console.dir)
 
-app.listen(port,()=>{
-    console.log("Listening port",port);
+app.listen(port, () => {
+    console.log("Listening port", port);
 })
+
+
